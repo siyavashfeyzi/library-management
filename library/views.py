67c9d77ce89962ed6b79f416881f053d8c4b9ee9
Siyavash Feyzi <58208265+siyavashfeyzi@users.noreply.github.com>
2023-02-19 12:56:49 +0330

    Add getAll entitys Views

--- library/views.py
+++ library/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
-
+from django.http import HttpResponse
+from . import models
 # Create your views here.
 
 
 def index(request):
     return render(request, "index.html", {"input": "library"})
+
+
+def getAllBooks(request):
+    query_set = models.Book.objects.all()
+
+    for book in query_set:
+        print(book)
+
+    return render(request, "getAllBooks")
+
+
+def getAllMembers(request):
+    query_set = models.Member.objects.all()
+
+    for member in query_set:
+        print(member)
+
+    return render(request, "getAllMember")
+
+
+def getAllOrders(request):
+    query_set = models.Order.objects.all()
+
+    for order in query_set:
+        print(order)
+
+    return render(request, "getAllOrders")
